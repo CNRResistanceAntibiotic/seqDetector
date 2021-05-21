96bb84490423e79d44c6a34252528cfbc444aa4b
Nilad <lezebre9@hotmail.fr>
2021-05-21 14:44:03 +0200

    commit

--- seqdetector/blast_functions.py
+++ seqdetector/blast_functions.py
@@ -42,15 +42,16 @@ def load_blastn_result(result_file, target_file, pass_pid=70, pass_pcv=70):
             q_id = data['qid']
             t_id = data['tid']
             for item in target_dic[t_id].description.split(';'):
-                print(item)
-                try:
-                    key, value = item.split(':')
-                    if key == 'dna_snp':
-                        key = 'known_dna_snp'
-                    t_des[key] = value
-                except Exception as e:
-                    print(e)
-                    t_des = {}
+                if item:
+                    print(item)
+                    try:
+                        key, value = item.split(':')
+                        if key == 'dna_snp':
+                            key = 'known_dna_snp'
+                        t_des[key] = value
+                    except Exception as e:
+                        print(e)
+                        t_des = {}
             item_dic = {'seqtype': 'dna', 'func': 'divers', 'mech': 'divers', 'known_dna_snp': ''}
             for item in item_dic.keys():
                 if item in t_des.keys():
